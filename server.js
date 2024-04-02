d69ee1727d35e354aa39287a6cd59b725cead3ee
Cecilia Lindström <cecilia.a.lindstrom@live.se>
2024-04-02 13:28:13 +0200

    Raderafunktion in the making

--- server.js
+++ server.js
@@ -16,9 +16,15 @@ app.use(express.static("public"));
 
 // routing
 app.get("/", (req, res) => {
-    res.render("index");
+    db.all("SELECT * FROM courses", (err, rows) => {
+        if (err) {
+            console.error("Error fetching courses:", err.message);
+            res.render("error", { error: "Det uppstod ett fel när kurserna skulle hämtas." });
+        } else {
+            res.render("index", { courses: rows });
+        }
+    });
 });
-
 app.get("/about", (req, res) => {
     res.render("about");
 });
@@ -55,7 +61,25 @@ app.post("/add-course", (req, res) => {
       res.send("Det uppstod ett fel när kursen skulle läggas till.");
     } else {
       console.log("Course added successfully. Course ID:", this.lastID);
-      res.send("Kursen har lagts till framgångsrikt.");
+      res.redirect("/");
     }
+
   });
+});
+
+
+//ta bort lagrad data
+app.post("/delete-course", (req, res) => {
+    const courseId = req.body.courseId;
+    console.log("Received courseId for deletion:", courseId);
+    db.run("DELETE FROM courses WHERE id = ?", [courseId], function(err) {
+        if (err) {
+            console.error("Error deleting course:", err.message);
+            res.send("Det uppstod ett fel när kursen skulle tas bort.");
+        } else {
+            console.log("Course deleted successfully. Course ID:", courseId);
+            // laddar om startsidan efter borttagning
+            res.redirect("/");
+        }
+    });
 });
